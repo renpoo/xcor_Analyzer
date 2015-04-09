0bbe4f1e9de4339495bb7ac17b59d51dae374587
renpoo <renpoo@Ariel.local>
2015-04-09 15:40:03 +0900

    Huge amount of refactoring is done.
    Now I can save(override) the setting CSV file.

---
+++ def_handles.m
@@ -0,0 +1,40 @@
+handles.flagsdata.funcFlag              = 1;
+handles.flagsdata.normalizeFlag       = 1;
+handles.flagsdata.nacfSingleFlag      = 0;
+handles.flagsdata.calcTauE_VecFlag = 0;
+handles.flagsdata.dumpFlag             = 0;
+handles.flagsdata.debugFlag            = 1;
+handles.flagsdata.debugStepFlag      = 0;
+handles.flagsdata.plotFlag               = 1;
+handles.flagsdata.plot3dFlag            = 1;
+handles.flagsdata.playSoundFlag      = 0;
+handles.flagsdata.exitFlag               = 0;
+
+handles.flagsdata.graphTitle            = 'Graph Title';
+handles.flagsdata.time_T                 = 1.0;
+handles.flagsdata.tS0                     = 0.0;
+handles.flagsdata.tE0                     = 1.0;
+handles.flagsdata.tStart                 = -0.005;
+handles.flagsdata.tStop                  = +0.005;
+handles.flagsdata.clipVal                 = 0.2;
+handles.flagsdata.Fs                       = 44100;
+
+handles.flagsdata.xLabelStr             = 'Time';
+handles.flagsdata.yLabelStr             = 'Time';
+handles.flagsdata.xUnitStr              = 'sec';
+handles.flagsdata.yUnitStr              = 'sec';
+
+handles.flagsdata.pname                 = '';
+handles.flagsdata.fname                  = '';
+handles.flagsdata.wavFileName        = '';
+handles.flagsdata.defCsvFileName    = '';
+
+handles.flagsdata.chDefs                = {};
+handles.flagsdata.ch                      = {};
+handles.flagsdata.csvFileNames       = {};
+handles.flagsdata.tmpText_chDefs   = {};
+
+handles.flagsdata.nacfAndoFlag        = 0;
+handles.flagsdata.phiFlag                 = 1;
+
+handles.flagsdata.numberOfHeaders = 5;
