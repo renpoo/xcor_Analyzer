0b3cd79cc6f692d9cecac620e67ac1659d5145bc
renpoo <renpoo@Ariel.local>
2015-09-11 01:25:16 +0900

    Temporary Commit after Seminar.
    Make them DRY!

--- batch_generateCombinationalSounds.m
+++ batch_generateCombinationalSounds.m
@@ -7,6 +7,7 @@ generateCombinationalSounds( './_Sounds', 'sine8192Hz_0deg.wav', 'sine8292Hz_0de
 generateCombinationalSounds( './_Sounds', 'sine8192Hz_0deg.wav', 'sine8320Hz_0deg.wav' );
 %}
 
+%{
 generateCombinationalSounds( './_Sounds', 'sine512Hz_0deg.wav',   'sine1024Hz_0deg.wav' );
 generateCombinationalSounds( './_Sounds', 'sine576Hz_0deg.wav',   'sine1024Hz_0deg.wav' );
 generateCombinationalSounds( './_Sounds', 'sine640Hz_0deg.wav',   'sine1024Hz_0deg.wav' );
@@ -15,3 +16,33 @@ generateCombinationalSounds( './_Sounds', 'sine768Hz_0deg.wav',   'sine1024Hz_0d
 generateCombinationalSounds( './_Sounds', 'sine864Hz_0deg.wav',   'sine1024Hz_0deg.wav' );
 generateCombinationalSounds( './_Sounds', 'sine960Hz_0deg.wav',   'sine1024Hz_0deg.wav' );
 generateCombinationalSounds( './_Sounds', 'sine1023Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%}
+
+%generateCombinationalSounds( './_Sounds', 'sine1000Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine998Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine996Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine994Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine992Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine990Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine988Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine986Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine984Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine982Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine980Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine978Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine976Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine974Hz_0deg.wav',  'sine1024Hz_0deg.wav' );
+
+%generateCombinationalSounds( './_Sounds', 'sine9950Hz_0deg.wav',  'sine10000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19950Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19945Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19890Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19780Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19560Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19340Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19414Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19505Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19450Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19257_5Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+%generateCombinationalSounds( './_Sounds', 'sine19175Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
+generateCombinationalSounds( './_Sounds', 'sine19120Hz_0deg.wav',  'sine20000Hz_0deg.wav' );
